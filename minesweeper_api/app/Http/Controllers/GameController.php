960692589fea94471e81575e3960f7da405c3bf4
JerryUam <gerardohdz9@gmail.com>
2020-08-15 01:37:38 -0500

    Mines flags in first minesweeper board aproach.

--- minesweeper_api/app/Http/Controllers/GameController.php
+++ minesweeper_api/app/Http/Controllers/GameController.php
@@ -16,10 +16,12 @@ class GameController extends Controller
         $gameLevelData=$this->objGameLevelsController->show($request->input('game_level_id'));//Load game level data by game level id
 
         $minesCoordinates=[];//Array for saving de mines coordinates (x,y)
+        $minesweeperBoard=[];//Array for saving de minesweeper game board representation
+
         do{
             $mineCoordinatesUsed=false;//Helper for cheking if there is a x,y coordinates for a mine
-            $mineColNumber=rand(0,$gameLevelData->game_level_colums);//x coordinate for a mine
-            $mineRowNumber=rand(0,$gameLevelData->game_level_rows);//y coordinate for a mine
+            $mineColNumber=rand(0,$gameLevelData->game_level_colums-1);//x coordinate for a mine
+            $mineRowNumber=rand(0,$gameLevelData->game_level_rows-1);//y coordinate for a mine
             foreach($minesCoordinates as $key => $mineCoordinates){
                 if($mineCoordinates==[$mineColNumber,$mineRowNumber]){//x,y coordinates validation, if true there already is a x,y with the same values
                     $mineCoordinatesUsed=true;
@@ -31,7 +33,26 @@ class GameController extends Controller
             }
         }while(count($minesCoordinates)<$gameLevelData->game_level_mines);
 
-        return var_dump($minesCoordinates);
+        for($x=0;$x<$gameLevelData->game_level_colums;$x++){//Go over x coordinantes
+            $row=[];
+            for($y=0;$y<$gameLevelData->game_level_rows;$y++){//Go over y coordinates
+                $thereIsAMine=false;//Helper for checking if there is a mine
+                foreach($minesCoordinates as $key => $mineCoordinates){//Foreach mine coordinates we check if border coordinate should be flaged with a mine
+                    if($mineCoordinates==[$x,$y]){//Theres is a mine in these x,y coordinates!!!
+                        $thereIsAMine=true;
+                        break;
+                    }
+                }
+                if($thereIsAMine){//If there is a mine it's flaged with M
+                    $row[]=["M"];
+                }else{//If not it's flaged with x,y coordinates
+                    $row[]=[$x,$y];
+                }
+            }
+            $minesweeperBoard[]=$row;//Each row is filled with colums data
+        }
+
+        return $minesweeperBoard;
     }
     
 }
